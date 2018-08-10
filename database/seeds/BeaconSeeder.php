57764d301a9d2ce2bcb845c29fd9621cd6e4badf
sumasi52 <sumasi52@gmail.com>
2018-08-10 14:20:49 +0900

    seeder 変更 ver1.3

--- database/seeds/BeaconSeeder.php
+++ database/seeds/BeaconSeeder.php
@@ -12,11 +12,11 @@ class BeaconSeeder extends Seeder
     public function run()
     {
         DB::table('beacons')->insert([
-            ['major' => '100', 'minor' => '100','position' => 'カフェスペース', ],
-            ['major' => '200', 'minor' => '200','position' => '会議室', ],
-            ['major' => '300', 'minor' => '300','position' => '会議室2', ],
-            ['major' => '333', 'minor' => '333','position' => '飯田橋駅', ],
-            ['major' => '400', 'minor' => '400','position' => '南国', ],
+            ['major' => '3333', 'minor' => '1', 'position' => 'カフェ' ],
+            ['major' => '3333', 'minor' => '2', 'position' => '会議室バンコク' ],
+            ['major' => '3333', 'minor' => '3', 'position' => '会議室マレーシア' ],
+            ['major' => '3333', 'minor' => '4', 'position' => '会議室ジャカルタ' ],
+            ['major' => '2222', 'minor' => '1111', 'position' => 'エントランス' ],
 
         ]);
     }
